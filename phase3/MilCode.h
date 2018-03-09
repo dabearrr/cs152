67d2e44e331ab6b5c547afcaa97202df3c0ba700
Jonathan Herrera <jherr029@bolt.cs.ucr.edu>
2018-03-09 12:34:31 -0800

    added statements

--- phase3/MilCode.h
+++ phase3/MilCode.h
@@ -8,87 +8,88 @@ class MilCode {
 	}
 	// Function Related Statements
 	string param(string name) {
-	
+        return ("param " + name);
 	}
 
 	string call(string name, string dest) {
-	
+        return ("call " + name + " " + dest);
 	}
 
 	string ret(string src) {
-	
-	}
-	// Variable Declaration Statements
-	string varName(string name) {
-		
+        return ("ret" + src)
 	}
 
-	string arrayName(string name) {
-		
+	// Variable Declaration Statements
+	string varName(string name) {
+	    return (". " + name);
+	}
+
+	string arrayName(string namem, int n) {
+	    return (".[] " + ", " + n);
 	}
 
 	// Copy Statements
 	string copyElement(string dst, string src) {
-	
+        return ("= ", + dst + ", " + src);
 	}
 
 	// Array Access Statements
 	string arrayAccessAssignFrom(string dst, string src, string index) {
-		
+	    return ("=[] ", + dst, ", " + src ", " + index);
 	}
 
 	string arrayAccessAssignTo(string dst, string index, string src) {
-		
+	    return ("[]=" + dst + ", " + index ", " + src );
 	}
 
 	// Input/Output Statements
 	string readIntoDest(string dst) {
-		
+	    return (".< " + dst);
 	}
 
 	string readIntoDestArray(string dst, string index) {
-		
+	    return (".[]< " + dst ", " + index);
 	}
 
 	string writeSourceToOut(string src) {
-		
+	    return (".> " + src);
 	}
 
 	string writeSourceArrayToOut(string src, string index) {
-		
+	    return (".[]> " + src + ", " + index);
 	}
 
 	// Arithmetic Operator Statements (one or both source operands can be immediates)
 	string arithmatic(string sign, string dst, string src1, string src2) {
-		
+	    return (sign + " " + dst + ", " + src1 ", " + src2);
 	}
 
 	// Comparison Operator Statements (one or both source operands can be immediates)
 	string comparison(string comp, string dst, string src1, string src2) {
-		
+	    return (comp + " " + dst + ", " + src1 + ", " src2);
 	}
 
 	// Logical Operator Statements (one or both source operands can be immediates)
 	string logicalBinary(string op, string dst, string src1, string src2) {
-		
+	    return (op + " " + dst + "," + src1 + ", " + src2);
 	}
 
 	string logicalNot(string dst, string src) {
-		
+	    return ("!" + dst + ", " + src);
 	}
 
 	// Label Declaration Statements
 	string label(string label) {
-		
+	    return (": " + label);
 	}
 
 	// Branch Statements
 	string gotoLabel(string label) {
-		
+	    return (":= " + label);
 	}
 
 	string condGotoLabel(string label, string predicate) {
-		
+	    return ("?:= " + label + ", " + predicate);
 	}
 
 	private:
