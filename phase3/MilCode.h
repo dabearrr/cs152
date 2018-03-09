b37200ba184c7d1709bb6708bb1765206cbb1c93
Jonathan Herrera <jherr029@bolt.cs.ucr.edu>
2018-03-09 12:12:59 -0800

    my first commit

--- phase3/MilCode.h
+++ phase3/MilCode.h
@@ -10,9 +10,11 @@ class MilCode {
 	string param(string name) {
 	
 	}
+
 	string call(string name, string dest) {
 	
 	}
+
 	string ret(string src) {
 	
 	}
@@ -20,59 +22,75 @@ class MilCode {
 	string varName(string name) {
 		
 	}
+
 	string arrayName(string name) {
 		
 	}
+
 	// Copy Statements
 	string copyElement(string dst, string src) {
 	
 	}
+
 	// Array Access Statements
 	string arrayAccessAssignFrom(string dst, string src, string index) {
 		
 	}
+
 	string arrayAccessAssignTo(string dst, string index, string src) {
 		
 	}
+
 	// Input/Output Statements
 	string readIntoDest(string dst) {
 		
 	}
+
 	string readIntoDestArray(string dst, string index) {
 		
 	}
+
 	string writeSourceToOut(string src) {
 		
 	}
+
 	string writeSourceArrayToOut(string src, string index) {
 		
 	}
+
 	// Arithmetic Operator Statements (one or both source operands can be immediates)
 	string arithmatic(string sign, string dst, string src1, string src2) {
 		
 	}
+
 	// Comparison Operator Statements (one or both source operands can be immediates)
 	string comparison(string comp, string dst, string src1, string src2) {
 		
 	}
+
 	// Logical Operator Statements (one or both source operands can be immediates)
 	string logicalBinary(string op, string dst, string src1, string src2) {
 		
 	}
+
 	string logicalNot(string dst, string src) {
 		
 	}
+
 	// Label Declaration Statements
 	string label(string label) {
 		
 	}
+
 	// Branch Statements
 	string gotoLabel(string label) {
 		
 	}
+
 	string condGotoLabel(string label, string predicate) {
 		
 	}
+
 	private:
 	
 };
